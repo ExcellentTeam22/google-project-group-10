fc52fbe02492ccd6f4ad89673320360c34879e48
EliadKarni <EliadK995@Gmail.com>
2022-07-25 14:03:19 +0300

    Project base

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-if __name__ == '__main__':
-    print("hello")
+from typing import List
+from AutoCompleteData import AutoCompleteData
+
+def get_best_k_completions(prefix: str) -> List[AutoCompleteData]:
+    return [AutoCompleteData]
+
+if __name__ == "__main__":
+    print("run")
