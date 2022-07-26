e506e12959e1a612343a00066584cef24cbcb4c5
Eliad Karni <EliadK995@Gmail.com>
2022-07-26 15:40:32 +0300

    Trie prefix search

--- LettersTrie.py
+++ LettersTrie.py
@@ -7,25 +7,34 @@ class LettersTrie:
         self.prev = prev
         self.content = set()
 
-    def append(self, new_word: LettersTrie, path: str, line: str) -> None:
+    def insert(self, new_word: LettersTrie, path: str, line: str) -> None:
         if len(new_word) == 0:
-            self.content.add(f"{path} \n {line}")
+            self.content.add(f"{path}\n{line}")
         else:
             if new_word[0] not in self.next:
                 self.next[new_word[0]] = LettersTrie(self)
-            self.next[new_word[0]].append(new_word[1:], path, line)
+            self.next[new_word[0]].insert(new_word[1:], path, line)
 
-    def get(self, word: str, distance: int = 0) -> list:
+    def get(self, word: str, pre: bool = False) -> set:
         if word == "":
+            if pre:
+                return self.get_children_contents()
             return self.content
-        if word[0] not in self.next:
+        elif word[0] not in self.next:
             return None
         else:
             return self.next[word[0]].get(word[1:])
 
+    def get_children_contents(self) -> set:
+        ans = set()
+        for key in self.next:
+           ans = ans.union(self.next[key].get_children_contents())
+        ans = ans.union(self.content)
+        return ans
+
 
 if __name__ == "__main__":
     data = LettersTrie()
-    data.append("123", "111", "222")
-    data.append("123", "5555", "7777")
-    print(data.get("123"))
+    data.insert("123", "111", "222")
+    data.insert("123", "5555", "7777")
+    print(data.get("123", True))
