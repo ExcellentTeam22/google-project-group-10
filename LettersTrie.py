878938e0ebb724211fc27281f8529755f285ba86
Eliad Karni <EliadK995@Gmail.com>
2022-07-27 12:43:35 +0300

    Made Tries search method returns set

--- LettersTrie.py
+++ LettersTrie.py
@@ -31,34 +31,39 @@ class Trie (object):
         node.content.add(tuple(np.array([path, line])))
         node.is_common = True
 
-    def dfs_sub_trie(self, node, pre):
-        #if len(node.content) != 0:
-        #    yield node, pre + node.char
+    def dfs_sub_tries(self, node, pre):
         for child in node.children.values():
-            for node, word in self.dfs_sub_trie(child, pre + node.char):
+            for node, word in self.dfs_sub_tries(child, pre + node.char):
                 yield node, word
         return
 
     def search(self, searched_word, prefix: bool = False, start_node: TrieNode = None, penalty: int = 0):
         node = self.root if start_node is None else start_node
-        ans = []
+        ans = set()
+        if node.char == "W" and searched_word == "here":
+            print("whar")
         if searched_word == "":
             if len(node.content) != 0 and searched_word == "":
-                ans += [[node.char, node.content]]
+                ans.add((node.char, tuple(node.content)))
             if prefix:
-                for curr_node, curr_word_prefix in self.dfs_sub_trie(node, searched_word[:1]):
+                for curr_node, curr_word_prefix in self.dfs_sub_tries(node, searched_word[:1]):
                     print(curr_word_prefix)
-                    ans.append([curr_word_prefix, curr_node.content])
+                    ans.add([curr_word_prefix, curr_node.content])
             return ans
         elif penalty < MAX_PENALTY:
-            for node_key in node.children:
-                #add char
-                ans += self.search(searched_word, prefix, node.children[node_key], penalty + 1)
-                if searched_word != "" and node != searched_word[0]:
-                    #switch char
-                    ans += self.search(searched_word[1:], prefix, node.children[node_key], penalty + 1)
-            #del char
-            ans += self.search(searched_word[1:], prefix, node, penalty + 1)
+            ans = ans.union(self.get_penaltied_words(searched_word, prefix, node, penalty))
         if searched_word[0] in node.children:
-            ans += self.search(searched_word[1:], prefix, node.children[searched_word[0]], penalty)
+            ans = ans.union(self.search(searched_word[1:], prefix, node.children[searched_word[0]], penalty))
+        return ans
+
+    def get_penaltied_words(self, searched_word: str, prefix: bool, node: TrieNode, penalty: int):
+        ans = set()
+        for node_key in node.children:
+            # add char
+            ans = ans.union(self.search(searched_word, prefix, node.children[node_key], penalty + 1))
+            if searched_word != "" and node != searched_word[0]:
+                # switch char
+                ans = ans.union(self.search(searched_word[1:], prefix, node.children[node_key], penalty + 1))
+        # del char
+        ans = ans.union(self.search(searched_word[1:], prefix, node, penalty + 1))
         return ans
